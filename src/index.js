03ce74238d8806dc5b492f66b8dc805b1ad1172a
Nemo <nemo.spaske@live.com>
2021-02-11 10:28:54 +0100

    Redux action created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provide } from 'react-redux';
+import { configureStore } from './store';
 import App from './App.js';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+<Provider store={configureStore()}>
+<App />
+</Provider>, 
+document.getElementById('root'),
+);
