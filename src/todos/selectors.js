a8fbea49c79dca4a990e53b7970c88b82490b422
Nemo <nemo.spaske@live.com>
2021-02-24 21:23:07 +0100

    converting css modules to styled component

--- src/todos/selectors.js
+++ src/todos/selectors.js
@@ -7,3 +7,8 @@ export const getIncompleteTodos = createSelector(
     getTodos,
     (todos) => todos.filter(todo => !todo.isCompleted),
 );
+
+export const getCompletedTodos = createSelector(
+    getTodos,
+    (todos) => todos.filter(todo => todo.isCompleted),
+);
